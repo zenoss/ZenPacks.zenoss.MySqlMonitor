8b45ddfb02243343885fae9959a3c0690db5053b
Chet Luther <cluther@zenoss.com>
2009-04-27 15:45:05 +0000

    * Fixes #4079: Prevent MySqlMonitor from enabling user-disabled processes. Reviewed by cholden.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.MySqlMonitor@26767 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/MySqlMonitor/__init__.py
+++ ZenPacks/zenoss/MySqlMonitor/__init__.py
@@ -1,7 +1,7 @@
 ###########################################################################
 #
 # This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2008, Zenoss Inc.
+# Copyright (C) 2008, 2009, Zenoss Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published by
@@ -19,13 +19,36 @@ from Products.CMFCore.DirectoryView import registerDirectory
 if os.path.isdir(skinsDir):
     registerDirectory(skinsDir, globals())
 
-from Products.ZenModel.ZenPack import ZenPackBase
+import logging
+log = logging.getLogger("zen.MySqlMonitor")
 
+from Products.ZenModel.ZenPack import ZenPackBase
 class ZenPack(ZenPackBase):
-    """ MySqlMonitor loader
+    """
+    ZenPacks.zenoss.MySqlMonitor ZenPack loader.
     """
 
     packZProperties = [
             ('zMySqlUsername', 'zenoss', 'string'),
             ('zMySqlPassword', '', 'string'),
             ]
+
+    def install(self, app):
+        ZenPackBase.install(self, app)
+        self.enableDefaultProcessMonitoring(app.zport.dmd)
+
+    def upgrade(self, app):
+        ZenPackBase.upgrade(self, app)
+        self.enableDefaultProcessMonitoring(app.zport.dmd)
+
+    def enableDefaultProcessMonitoring(self, dmd):
+        try:
+            p = dmd.Processes.MySQL.osProcessClasses.mysqld
+            if p.hasProperty('zMonitor'): return
+            log.info('Enabling monitoring for mysqld processes.')
+            p._setProperty('zMonitor', True)
+            for i in p.instances():
+                i.index_object()
+        except AttributeError:
+            pass
+
