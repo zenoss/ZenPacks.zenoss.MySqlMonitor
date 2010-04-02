77a7a604cbb2de2a0eac69b43c4f1643f637a45c
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2010-04-02 17:04:05 +0000

    created info/interface objects, reviewed by John Causey
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.MySqlMonitor@31777 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/MySqlMonitor/info.py
@@ -0,0 +1,37 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+from zope.interface import implements
+from Products.Zuul.infos import ProxyProperty
+from Products.Zuul.infos.template import BasicDataSourceInfo
+from ZenPacks.zenoss.MySqlMonitor.interfaces import IMySqlMonitorDataSourceInfo
+
+
+class MySqlMonitorDataSourceInfo(BasicDataSourceInfo):
+    implements(IMySqlMonitorDataSourceInfo)
+    timeout = ProxyProperty('timeout')
+    usessh = ProxyProperty('usessh')
+    versionFivePlus = ProxyProperty('versionFivePlus')
+    hostname = ProxyProperty('hostname')
+    port = ProxyProperty('port')
+    username = ProxyProperty('username')
+    password = ProxyProperty('password')
+        
+    @property
+    def testable(self):
+        """
+        We can NOT test this datsource against a specific device
+        """
+        return False
+    
+
+
---
+++ ZenPacks/zenoss/MySqlMonitor/interfaces.py
@@ -0,0 +1,30 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2010, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+from Products.Zuul.interfaces import IBasicDataSourceInfo
+from Products.Zuul.form import schema
+from Products.Zuul.utils import ZuulMessageFactory as _t
+
+
+class IMySqlMonitorDataSourceInfo(IBasicDataSourceInfo):
+    usessh = schema.Bool(title=_t(u"Use SSH"))
+    cycletime = schema.Int(title=_t(u'Cycle Time (seconds)'))
+    timeout = schema.Int(title=_t(u'Timeout (seconds)'))
+    hostname = schema.Text(title=_t(u'MySQL Host'), group=_t(u'MySQL'))
+    username = schema.Text(title=_t(u'MySQL Username'), group=_t(u'MySQL'))
+    port = schema.Text(title=_t(u'MySQL Port'), group=_t(u'MySQL'))
+    password = schema.Password(title=_t(u'MySQL Password'), group=_t(u'MySQL'))
+    versionFivePlus = schema.Bool(title=_t(u'MySQL Version 5+'), group=_t(u'MySQL'))
+    
+    
+    
+
