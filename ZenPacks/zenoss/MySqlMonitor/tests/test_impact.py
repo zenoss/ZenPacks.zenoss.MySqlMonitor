d5ccdee929b86dc46a1602731c930c994a34bb92
Andrii Martyniuk <amartyn@softserveinc.com>
2013-10-18 15:18:33 +0300

    Ipmact tests.

---
+++ ZenPacks/zenoss/MySqlMonitor/tests/test_impact.py
@@ -0,0 +1,121 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import functools
+
+from zope.component import subscribers
+
+from Products.Five import zcml
+
+from Products.ZenTestCase.BaseTestCase import BaseTestCase
+from Products.ZenUtils.guid.interfaces import IGUIDManager
+from Products.ZenUtils.Utils import unused
+
+from ZenPacks.zenoss.MySqlMonitor.tests.utils import test_subscription
+
+def require_impact(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            import ZenPacks.zenoss.Impact
+            unused(ZenPacks.zenoss.Impact)
+        except ImportError:
+            return
+
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def impacts_for(thing):
+    '''
+    Return a two element tuple.
+
+    First element is a list of object ids impacted by thing. Second element is
+    a list of object ids impacting thing.
+    '''
+    try:
+        from ZenPacks.zenoss.Impact.impactd.interfaces import \
+            IRelationshipDataProvider
+
+    except ImportError:
+        return ([], [])
+
+    impacted_by = []
+    impacting = []
+
+    guid_manager = IGUIDManager(thing.getDmd())
+    for subscriber in subscribers([thing], IRelationshipDataProvider):
+        for edge in subscriber.getEdges():
+            source = guid_manager.getObject(edge.source)
+            impacted = guid_manager.getObject(edge.impacted)
+            if source == thing:
+                impacted_by.append(impacted.id)
+            elif impacted == thing:
+                impacting.append(source.id)
+
+    return (impacted_by, impacting)
+
+
+class TestImpact(BaseTestCase):
+    '''
+    Test suite for all Impact adapters.
+    '''
+
+    def afterSetUp(self):
+        super(TestImpact, self).afterSetUp()
+
+        try:
+            import ZenPacks.zenoss.DynamicView
+            zcml.load_config('configure.zcml', ZenPacks.zenoss.DynamicView)
+        except ImportError:
+            pass
+
+        try:
+            import ZenPacks.zenoss.Impact
+            zcml.load_config('meta.zcml', ZenPacks.zenoss.Impact)
+            zcml.load_config('configure.zcml', ZenPacks.zenoss.Impact)
+        except ImportError:
+            pass
+
+        import ZenPacks.zenoss.Microsoft.Azure
+        zcml.load_config('configure.zcml', ZenPacks.zenoss.MySqlMonitor)
+
+    def subscription(self):
+        if not hasattr(self, '_subscription'):
+            self._subscription = test_subscription(self.dmd, factor=1)
+
+        return self._subscription
+
+
+    @require_impact
+    def test_MySqlMonitorSubscriptionImpacts(self):
+        impacts, impacted_by = impacts_for(self.subscription())
+
+        # Database -> Subscription
+        self.assertTrue('database0' in impacted_by)
+        # Servers -> Subscription
+        self.assertTrue('server0' in impacted_by)
+
+
+    @require_impact
+    def test_MySqlMonitorServerImpacts(self):
+        sr = self.subscription().getObjByPath(
+            'servers/server0')
+
+        impacts, impacted_by = impacts_for(sr)
+        self.assertTrue('database0-0' in impacted_by)
+
+    
+    @require_impact
+    def test_MySqlMonitorDatabaseImpacts(self):
+        db = self.subscription().getObjByPath(
+            'servers/server0/databases/database0')
+    
+        
--- ZenPacks/zenoss/MySqlMonitor/tests/util.py
+++ ZenPacks/zenoss/MySqlMonitor/tests/util.py
@@ -13,7 +13,58 @@
 
 import os.path
 
+from zope.event import notify
+from Products.Zuul.catalog.events import IndexingEvent
+
+
 def load_data(filename):
     path = os.path.join(os.path.dirname(__file__), 'data', filename)
     with open(path, 'r') as f:
         return f.read()
+
+
+def add_obj(relationship, obj):
+    '''
+    Add obj to relationship, index it, then returns the persistent
+    object.
+    '''
+    relationship._setObject(obj.id, obj)
+    obj = relationship._getOb(obj.id)
+    obj.index_object()
+    notify(IndexingEvent(obj))
+    return obj
+
+
+def test_subscription(dmd, factor=1):
+    '''
+    Return an example MySqlMonitorSubscription with a full set of example components.
+    '''
+    from ZenPacks.zenoss.MySqlMonitor.MySQLDatabase import MySQLDatabase
+    from ZenPacks.zenoss.MySqlMonitor.MySQLServer import MySQLServer
+    
+
+    dc = dmd.Devices.createOrganizer('/MySqlMonitor/MySqlSubscription')
+    dc.setZenProperty('zPythonClass', 'ZenPacks.zenoss.MySqlMonitor.MySqlSubscription')
+
+    subscription = dc.createInstance('subscription')
+    subscription.setPerformanceMonitor('localhost')
+    subscription.linuxDeviceClass = '/Server/Linux'
+    subscription.index_object()
+    notify(IndexingEvent(subscription))
+
+
+        # Server
+        for server_id in range(factor):
+            server = add_obj(
+                subscription.servers,
+                MySQLServer('server%s' % (
+                    server_id)))
+
+                # Database
+                for database_id in range(factor):
+                    database = add_obj(
+                        subscription.databases,
+                        MySQLDatabase('database%s-%s' % (
+                            database_id)))
+
+        return subscription
